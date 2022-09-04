747cf7c29ad1b13b794b9c803ca4b0fef3f1d448
VelezPy <solrac.zelev@outlook.com>
2022-09-04 17:39:49 -0400

    Create ex_03_02.py
    This exercise introduces the keywords 'try' and 'except'.

---
+++ ex_03_02.py
@@ -0,0 +1,20 @@
+sh = input ("Enter Hours: ")
+sr = input ("Enter Rate: ")
+try:
+    fh = float(sh)
+    fr = float(sr)
+except:
+    print("Error, please enter numeric input")
+    quit()
+
+print(fh, fr)
+if fh > 40 :
+    #print("Overtime")
+    reg = fr * fh
+    otp = (fh - 40.0) * (fr * 0.5)
+    #print (reg, otp)
+    xp = reg + otp
+else:
+    #print("Regular")
+    xp = fh * fr
+print("Pay: ", xp)
