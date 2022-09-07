bc1a51e5b510bcff5d3aa32cc149451be5d3f540
VelezPy <solrac.zelev@outlook.com>
2022-09-07 18:05:45 -0400

    Update iterationsA.py
    These small 'while scripts' includes 'break' and 'continue'.

--- iterationsA.py
+++ iterationsA.py
@@ -5,3 +5,20 @@ while n > 0:
     n -= 1
 print('Blastoff!')
 print(n)
+
+while True: 
+    line = input('> ')
+    if line == 'done':
+        break
+    print(line)
+print('Done!')
+
+while True: 
+    line = input('> ')
+    if line[0] == '#' :
+        continue
+    if line == 'done' :
+        break
+    print(line)
+print('Done!')
+       
