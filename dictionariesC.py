496b5b077664b70e9d1325350e44f7ede31921ef
VelezPy <solrac.zelev@outlook.com>
2022-09-20 20:21:33 -0400

    Create dictionariesC.py
    It takes a line of text, breaks it down and outputs the most common word

---
+++ dictionariesC.py
@@ -0,0 +1,10 @@
+counts = {}
+line = input('Enter a line a of text: ')
+words = line.split()
+
+print('Words:', words)
+print('Counting...')
+
+for word in words:
+    counts[word] = counts.get(word, 0) + 1
+print('Counts', counts)
