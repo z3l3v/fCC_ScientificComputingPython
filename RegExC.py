d5223c12ca10216ea39e809f42c7bc904ba5fe57
VelezPy <solrac.zelev@outlook.com>
2022-10-04 18:33:28 -0400

    Create RegExC.py
    This script uses regular expressions to extract data from a text file.

---
+++ RegExC.py
@@ -0,0 +1,11 @@
+import re
+hand = open('mboxshort.txt')
+numlist = list()
+for line in hand:
+    line = line.rstrip()
+    stuff = re.findall('^X-DSPAM-Confidence: ([0-9.]+)', line)
+    if len(stuff) != 1 : continue
+    num = float(stuff[0])
+    numlist.append(num)
+print('Maximum:', max(numlist))
+    
