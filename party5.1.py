5f866c0616b02a810c303278208f646c9b9a9eef
VelezPy <solrac.zelev@outlook.com>
2022-11-04 18:04:43 -0400

    Create party5.1.py
    This script introduces the concept of inheritance in object-oriented programming.

---
+++ party5.1.py
@@ -0,0 +1,24 @@
+class PartyAnimal:
+    x = 0
+    name = ""
+    def __init__(self,z):
+        self.name = z
+        print(self.name, "constructed")
+        
+    def party(self):
+        self.x = self.x + 1
+        print(self.name, "party count", self.x)
+        
+class FootballFan(PartyAnimal):
+    points = 0
+    def touchdown(self):
+        self.points = self.points + 7
+        self.party()
+        print(self.name, "points", self.points)
+        
+s = PartyAnimal("Sally")
+s.party()
+
+j = PartyAnimal("Jim")
+j.party()
+s.party()
