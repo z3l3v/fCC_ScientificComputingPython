09053b7627fd4b9e4b75c9b7984076b25e9291c0
VelezPy <solrac.zelev@outlook.com>
2022-11-08 21:13:52 -0400

    Update emaildb.py
    Fixed some errors.

--- emaildb.py
+++ emaildb.py
@@ -3,30 +3,32 @@ import sqlite3
 conn = sqlite3.connect('emaildb.sqlite')
 cur = conn.cursor()
 
-cur.execute('''
-DROP TABLE IF EXISTS Counts''')
+cur.execute('DROP TABLE IF EXISTS Counts')
 
 cur.execute('''
-CREATE TABLLE Counts (email TEXT, count INTEGER)''')
+CREATE TABLE Counts (email TEXT, count INTEGER)''')
 
 fname = input('Enter file name: ')
 if (len(fname) < 1): fname = 'mboxshort.txt'
 fh = open(fname)
 for line in fh:
     if not line.startswith('From: '): continue
-    prieces = line.split()
+    pieces = line.split()
     email = pieces[1]
-    cur.execute('SELECT count FROM Counts WHERE email = 7 ', (email,))
+    cur.execute('SELECT count FROM Counts WHERE email = ? ', (email,))
     row = cur.fetchone()
     if row is None:
         cur.execute('''INSERT INTO Counts (email, count)
-                VALUES (7, 1)''', (email,))
+                VALUES (?, 1)''', (email,))
     else: 
-        cur.execute('UPDATE Counts SET count = count + 1 WHERE email = 7',
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE email = ?',
                      (email,))
     conn.commit()
 
 # https://www.sqlite.org/lang_select.html
 sqlstr = 'SELECT email, count FROM Counts ORDER by count DESC LIMIT 10'
 
-for rom in cur.execute(sqlstr)
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+
+cur.close()
