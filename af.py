a3e91c2da2eda330fbc4e1dd2a413ed1d882bf2a
VelezPy <solrac.zelev@outlook.com>
2022-11-28 17:33:17 -0400

    Update af.py

--- af.py
+++ af.py
@@ -13,29 +13,28 @@ def arithmetic_arranger(problems):
         top.append(m[0])
         bottom.append(m[2])
         operator.append(m[1])
-        list_of_lists.append(m)
+        list_of_lists = top + bottom
         
-    #print (list_of_lists)
         
     if "*" in operator or "/" in operator:
-        print ("Error: Operator must be '+' or '-'.")
+        return ("Error: Operator must be '+' or '-'.")
      
     for i in list_of_lists:
-        if int(i[0]) == False or int(i[2]) == False:
-            print ("Error: Numbers must only contain digits.")
+        if int(i) == False:
+            return ("Error: Numbers must only contain digits.")
             break
             
     for i in list_of_lists: 
-        if len(i[0]) > 4 or len(i[2]) > 4:
-            print ("Error: Numbers cannot be more than four digits.")
+        if len(i) > 4:
+            return ("Error: Numbers cannot be more than four digits.")
     
     top_row = '' 
     dashes = ''
     solutions = '' 
     values = list(map(lambda x: eval(x), problems))
     
-    for i in range(0, len(list_of_lists), 2):
-        length = max(len(list_of_lists[0]), len(list_of_lists[2])) + 2
+    for i in range(0, len(top), 2):
+        length = max(len(top[i]), len(top [i])) + 2
         top_row += top[i].rjust(length)
         dashes += '-' * length
         solutions += str(values[i // 2]).rjust(length)
@@ -43,8 +42,8 @@ def arithmetic_arranger(problems):
             top_row = ' ' * 4
             dashes += ' ' * 4
             solutions += ' ' * 4
-    print (top_row)
-                    
+        print (top_row)
+         
     '''
     #length = max(len(top[0]), len (bottom[2])) + 2
     #width = length +2 
@@ -54,13 +53,6 @@ def arithmetic_arranger(problems):
     dashes = '_' * width
     answers = str(int(lists_of_lists[0]} + int(list_of_lists[2]})
     a = f"{ans :>{width}}"
-    
-    for i in range(top):
     '''
-        
-    
-    
-        
-        
-    
+     
 arithmetic_arranger(["4212 + 89", "2 + 1", "23 + 45"])
